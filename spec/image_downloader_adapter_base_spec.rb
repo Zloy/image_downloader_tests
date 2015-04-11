03156dd025d438367b23b759a34563eab7578326
zloy <zhoran@inbox.ru>
2015-04-11 18:26:02 +0300

    Implement pending specs for ImageDownloaderAdapter::Base

---
+++ spec/image_downloader_adapter_base_spec.rb
@@ -0,0 +1,36 @@
+klass = ImageDownloaderAdapter::Base
+
+describe klass do
+  xit 'should download img tags with src in scheme http' do
+  end
+
+  xit 'should download img tags with src in scheme http' do
+  end
+
+  xit 'should download img tags with srcset in scheme http' do
+  end
+
+  xit 'should download img tags with src in scheme https' do
+  end
+
+  xit 'should download img tags with srcset in scheme https' do
+  end
+
+  xit 'should download img tags with embedded images' do
+  end
+
+  xit 'should download elements with background property in style attr' do
+  end
+
+  xit 'should download elements with background-image property in style attr' do
+  end
+
+  xit 'should download elements with background property in stylesheet' do
+  end
+
+  xit 'should download elements with background-image property in stylesheet' do
+  end
+
+  xit 'should download lazily loaded elements' do
+  end
+end
